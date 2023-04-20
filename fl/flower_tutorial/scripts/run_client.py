d8da021a77ed8d19b09403309b293a48e2c4dcf5
Jan Przybyszewski <jasiek.przybyszewski@gmail.com>
2023-04-20 21:33:44 +0200

    Allow to configure server address through args. Add debug logs.

--- fl/flower_tutorial/scripts/run_client.py
+++ fl/flower_tutorial/scripts/run_client.py
@@ -1,11 +1,24 @@
-import flwr
-from core.datasets.cifar_10 import CIFAR10
+import argparse
+import socket
 
+import flwr
+
+from core.datasets.cifar_10 import CIFAR10
 from core.models.cifar_10.SimpleCNN import SimpleCNN
 from fl.flower_tutorial.actors.simple_client import CifarClient
+
+
+parser = argparse.ArgumentParser("federated_server")
+parser.add_argument("--server-ip", dest="server_ip",
+                    help="ip of the server", default="localhost")
+args = parser.parse_args()
+
+server_hostname = socket.gethostname()
+print("Server hostname: ", server_hostname)
+print("Server address: ", socket.gethostbyname(server_hostname))
 
 DEFAULT_DATASET_ROOT = "./data/raw/cifar_10"
 net = SimpleCNN()
 dataset = CIFAR10(root=DEFAULT_DATASET_ROOT)
 flwr.client.start_numpy_client(
-    server_address="[::]:8080", client=CifarClient(net, dataset))
+    server_address=f"{args.server_ip}:8081", client=CifarClient(net, dataset))
--- fl/flower_tutorial/scripts/run_server.py
+++ fl/flower_tutorial/scripts/run_server.py
@@ -1,6 +1,18 @@
+import argparse
+import socket
+
 import flwr as fl
 
 from core.utils.cifar_10 import weighted_average
+
+parser = argparse.ArgumentParser("federated_client")
+parser.add_argument("--server-ip", dest="server_ip",
+                    help="ip of the server", default="localhost")
+args = parser.parse_args()
+
+server_hostname = socket.gethostname()
+print("Server hostname: ", server_hostname)
+print("Server address: ", socket.gethostbyname(server_hostname))
 
 strategy = fl.server.strategy.FedAvg(
     fraction_evaluate=1.0,
@@ -9,5 +21,5 @@ strategy = fl.server.strategy.FedAvg(
 )
 
 fl.server.start_server(
-    server_address="[::]:8080", config=fl.server.ServerConfig(num_rounds=3), strategy=strategy,
+    server_address=f"{args.server_ip}:8081", config=fl.server.ServerConfig(num_rounds=3), strategy=strategy,
 )
