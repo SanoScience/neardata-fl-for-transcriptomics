b0b6a7ebe5ca258edaede5c0a43834ecd75d1eea
Jan Przybyszewski <jasiek.przybyszewski@gmail.com>
2023-06-10 00:58:38 +0200

    Add data split service. Integrate with flower_tutorial example.

--- core/datasets/cifar_10.py
+++ core/datasets/cifar_10.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import List, TypedDict
 
 import torch
 import torchvision
@@ -12,22 +12,34 @@ class NumExamples(TypedDict):
 
 
 class CIFAR10:
-    def __init__(self, root: str) -> None:
+    def __init__(self, root: str, data_split_indices: List[int], train_test_ratio: float = 0.7) -> None:
         transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
 
-        self.trainset = torchvision.datasets.CIFAR10(
-            root=root, train=True, download=True, transform=transform)
-        self.testset = torchvision.datasets.CIFAR10(
-            root=root, train=False, download=True, transform=transform)
+        self.full_dataset = self._get_full_dataset(root, transform)
+
+        n_train_samples = round(len(data_split_indices)*train_test_ratio)
+        self.train_indices = data_split_indices[:n_train_samples]
+        self.test_indices = data_split_indices[n_train_samples:]
 
     def get_trainloader(self, batch_size: int = 32, shuffle: bool = True, num_workers: int = 2) -> DataLoader:
-        return torch.utils.data.DataLoader(self.trainset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+        train_subset = torch.utils.data.Subset(
+            self.full_dataset, self.train_indices)
+        return torch.utils.data.DataLoader(train_subset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
     def get_testloader(self, batch_size: int = 32, shuffle: bool = True, num_workers: int = 2) -> DataLoader:
-        return DataLoader(self.testset,  batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+        test_subset = torch.utils.data.Subset(
+            self.full_dataset, self.test_indices)
+        return DataLoader(test_subset,  batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
     def get_num_examples(self) -> NumExamples:
-        return NumExamples(trainset=len(self.trainset), testset=len(self.testset))
+        return NumExamples(trainset=len(self.train_indices), testset=len(self.test_indices))
+
+    def _get_full_dataset(self, root: str, transform: transforms.Compose) -> torchvision.datasets.CIFAR10:
+        trainset = torchvision.datasets.CIFAR10(
+            root=root, train=True, download=True, transform=transform)
+        testset = torchvision.datasets.CIFAR10(
+            root=root, train=False, download=True, transform=transform)
+        return torch.utils.data.ConcatDataset([trainset, testset])
--- fl/flower_tutorial/scripts/run_client.py
+++ fl/flower_tutorial/scripts/run_client.py
@@ -1,9 +1,11 @@
 import argparse
+import json
 import socket
 import secrets
 import logging
 
 import flwr
+import requests
 
 from core.datasets.cifar_10 import CIFAR10
 from core.models.cifar_10.SimpleCNN import SimpleCNN
@@ -16,6 +18,8 @@ logger.setLevel(logging.INFO)
 parser = argparse.ArgumentParser("federated_client")
 parser.add_argument("--server-ip", dest="server_ip",
                     help="ip of the server", default="localhost")
+parser.add_argument("--data-split-server-ip", dest="data_split_server_ip",
+                    help="ip of the data split server", default="localhost")
 parser.add_argument("--client-id", dest="client_id",
                     help="unique client id", default=secrets.token_hex(8))
 
@@ -26,8 +30,13 @@ logger.info(f"Server hostname: {server_hostname}")
 logger.info(f"Server address: {socket.gethostbyname(server_hostname)}")
 logger.info(f"Client ID: {args.client_id}")
 
+data_split_server_endpoint = f"http://{args.data_split_server_ip}:8080/get_data_split"
+data_split_key = "split_indices"
+data_split_indices = json.loads(requests.get(data_split_server_endpoint).json()[data_split_key])
+
 DEFAULT_DATASET_ROOT = "./data/raw/cifar_10"
+dataset = CIFAR10(root=DEFAULT_DATASET_ROOT, data_split_indices=data_split_indices)
+
 net = SimpleCNN()
-dataset = CIFAR10(root=DEFAULT_DATASET_ROOT)
 flwr.client.start_numpy_client(
     server_address=f"{args.server_ip}:8081", client=CifarClient(net, dataset, args.client_id))
